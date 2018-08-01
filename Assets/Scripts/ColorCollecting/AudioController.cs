e2cb2e96d4f66f73c0fc0c24d38b83eb3c546914
Unknown <pisitpong.won@gmail.com>
2018-08-02 03:12:29 +0900

    Fixed Audio Light and Color changing

--- Assets/Scripts/ColorCollecting/AudioController.cs
+++ Assets/Scripts/ColorCollecting/AudioController.cs
@@ -20,25 +20,25 @@ public class AudioController : MonoBehaviour
 		if(state==1&&player.transform.position.x>=14&&player.transform.position.x<=80)
 		{
 			ad.clip=forestAudio;
-			ad.Play();
+			ad.Play();print("play1");
 			state=2;
 		}
 		if(state==2&&player.transform.position.x>=105&&player.transform.position.x<=174)
 		{
 			ad.clip=winterAudio;
-			ad.Play();
+			ad.Play();print("play2");
 			state=3;
 		}
 		if(state==3&&player.transform.position.x>=213&&player.transform.position.x<=286)
 		{
 			ad.clip=desertAudio;
-			ad.Play();
+			ad.Play();print("play3");
 			state=4;
 		}
 		if(state==4&&player.transform.position.x>=314&&player.transform.position.x<=386)
 		{
 			ad.clip=oceanAudio;
-			ad.Play();
+			ad.Play();print("play4");
 			state=5;
 		}
 	}
