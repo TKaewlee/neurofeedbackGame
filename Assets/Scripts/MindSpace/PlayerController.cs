420d0c6553e1caf96b0b304bb99196e4e9bdec1c
Nonthaporn Nakphu <43634056+farm-nakphu@users.noreply.github.com>
2019-09-17 16:53:25 +0900

    Update Fixed Timestep
    From 0.02 to 0.01333, to make game smoother

--- Assets/Scripts/MindSpace/PlayerController.cs
+++ Assets/Scripts/MindSpace/PlayerController.cs
@@ -54,21 +54,21 @@ public class PlayerController : MonoBehaviour
 
 	void FixedUpdate ()
 	{
-			float moveHorizontal = Input.GetAxis("Horizontal");
-			float moveVertical = Input.GetAxis("Vertical");
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
 
-			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-			GetComponent<Rigidbody>().velocity = movement * speed;
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		GetComponent<Rigidbody>().velocity = movement * speed;
 
-			GetComponent<Rigidbody>().position = new Vector3 
-			(
-				Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
-				0.0f, 
-				Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
-			);
+        GetComponent<Rigidbody>().position = new Vector3 
+		(
+			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+		);
 
-			GetComponent<Rigidbody>().rotation = 
-			Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+		GetComponent<Rigidbody>().rotation = 
+		Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
 
 	}
 }
